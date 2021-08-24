5443bb3616c7593e6676a11ac830889d8abc7a3d
Summers Pittman <supittma@redhat.com>
2021-08-24 17:31:04 -0400

    todo: fixup

---
+++ examples/src/test/java/com/redhat/cloud/ServiceRegistryIT.java
@@ -0,0 +1,37 @@
+package com.redhat.cloud;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.openshift.cloud.api.srs.*;
+import com.openshift.cloud.api.srs.invoker.*;
+import com.openshift.cloud.api.srs.invoker.auth.*;
+import com.openshift.cloud.api.srs.models.*;
+
+
+public class ServiceRegistryIT {
+
+    @Test
+    public void testListExample() {
+        var defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath("http://localhost:8080");
+        
+        var oauth = (HttpBearerAuth) defaultClient.getAuthentication("Bearer");
+        oauth.setBearerToken("bearerToken");
+
+        var apiInstance = new RegistriesApi(defaultClient);
+        
+        try {
+            RegistryListRest result = apiInstance.getRegistries(null, null, null, null);
+            assertNotNull(result);
+            assertEquals(1, result.getItems().size());
+        } catch (ApiException e) {
+            System.err.println("Exception when calling RegistriesApi#getRegistries");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+}
