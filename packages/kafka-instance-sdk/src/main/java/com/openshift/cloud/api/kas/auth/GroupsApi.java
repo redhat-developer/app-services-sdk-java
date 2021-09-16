60fd92dcf9015d85dc6d58b13d04dfdceafd0f03
Red Hat Application Services CI <84090353+app-services-ci@users.noreply.github.com>
2021-09-16 14:44:26 +0100

    chore(all): re-generate OpenAPI client(s) (#99)
    Co-authored-by: app-services-ci <app-services-ci@users.noreply.github.com>

--- packages/kafka-instance-sdk/src/main/java/com/openshift/cloud/api/kas/auth/GroupsApi.java
+++ packages/kafka-instance-sdk/src/main/java/com/openshift/cloud/api/kas/auth/GroupsApi.java
@@ -136,9 +136,7 @@ public class GroupsApi {
   /**
    * List of consumer groups in the Kafka instance.
    * Returns a list of all consumer groups for a particular Kafka instance. The consumer groups returned are limited to those records the requestor is authorized to view.
-   * @param offset The page offset (optional)
    * @param limit Maximum number of consumer groups to return (optional)
-   * @param size Maximum number of consumer groups to return on single page (optional)
    * @param page The page when returning the list of consumer groups (optional)
    * @param topic Return consumer groups where the topic name contains with this value (optional)
    * @param groupIdFilter Return the consumer groups where the ID contains with this value (optional)
@@ -147,7 +145,7 @@ public class GroupsApi {
    * @return a {@code ConsumerGroupList}
    * @throws ApiException if fails to make API call
    */
-  public ConsumerGroupList getConsumerGroups(Integer offset, Integer limit, Integer size, Integer page, String topic, String groupIdFilter, String order, String orderKey) throws ApiException {
+  public ConsumerGroupList getConsumerGroups(Integer limit, Integer page, String topic, String groupIdFilter, String order, String orderKey) throws ApiException {
     Object localVarPostBody = null;
     
     // create path and map variables
@@ -159,9 +157,7 @@ public class GroupsApi {
     Map<String, String> localVarCookieParams = new HashMap<String, String>();
     Map<String, Object> localVarFormParams = new HashMap<String, Object>();
 
-    localVarQueryParams.addAll(apiClient.parameterToPairs("", "offset", offset));
     localVarQueryParams.addAll(apiClient.parameterToPairs("", "limit", limit));
-    localVarQueryParams.addAll(apiClient.parameterToPairs("", "size", size));
     localVarQueryParams.addAll(apiClient.parameterToPairs("", "page", page));
     localVarQueryParams.addAll(apiClient.parameterToPairs("", "topic", topic));
     localVarQueryParams.addAll(apiClient.parameterToPairs("", "group-id-filter", groupIdFilter));
