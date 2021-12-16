a190603859d1ee2245ed86b0ad2877297d11883e
app-services-ci <app-services-ci@users.noreply.github.com>
2021-12-16 09:32:33 +0000

    chore(all): re-generate OpenAPI client(s)

--- packages/registry-management-sdk/src/main/java/com/openshift/cloud/api/srs/ErrorsApi.java
+++ packages/registry-management-sdk/src/main/java/com/openshift/cloud/api/srs/ErrorsApi.java
@@ -74,7 +74,7 @@ public class ErrorsApi {
     };
     final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
 
-    String[] localVarAuthNames = new String[] { "Bearer" };
+    String[] localVarAuthNames = new String[] {  };
 
     GenericType<Error> localVarReturnType = new GenericType<Error>() {};
     return apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
@@ -115,7 +115,7 @@ public class ErrorsApi {
     };
     final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
 
-    String[] localVarAuthNames = new String[] { "Bearer" };
+    String[] localVarAuthNames = new String[] {  };
 
     GenericType<ErrorList> localVarReturnType = new GenericType<ErrorList>() {};
     return apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
