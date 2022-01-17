3f2c1765149b9b7a0844649a5d777d5e0261fcbc
Hoyt Summers Pittman <secondsun@gmail.com>
2022-01-17 10:17:31 -0500

    [WIP] test: creating verification tests project (#87)
    * test: creating verification tests project
    
    * chore: correcting java version
    
    * tesT: adding more tests
    
    * todo: fixup

---
+++ examples/src/test/java/com/redhat/cloud/KafkaManagementIT.java
@@ -0,0 +1,61 @@
+package com.redhat.cloud;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import com.openshift.cloud.api.kas.invoker.*;
+import com.openshift.cloud.api.kas.invoker.auth.*;
+import com.openshift.cloud.api.kas.models.*;
+import com.openshift.cloud.api.kas.DefaultApi;
+
+public class KafkaManagementIT {
+    @Test
+    public void testCreateExample() {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath("http://localhost:8080");
+        
+        // Configure HTTP bearer authorization: Bearer
+        HttpBearerAuth Bearer = (HttpBearerAuth) defaultClient.getAuthentication("Bearer");
+        Bearer.setBearerToken("BEARER TOKEN");
+
+        DefaultApi apiInstance = new DefaultApi(defaultClient);
+        Boolean async = true; // Boolean | Perform the action in an asynchronous manner
+        KafkaRequestPayload kafkaRequestPayload = new KafkaRequestPayload(); // KafkaRequestPayload | Kafka data
+        kafkaRequestPayload.setName("name");
+        try {
+            KafkaRequest result = apiInstance.createKafka(async, kafkaRequestPayload);
+            assertNotNull(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling DefaultApi#createKafka");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testListExample() {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath("http://localhost:8080");
+        
+        // Configure HTTP bearer authorization: Bearer
+        HttpBearerAuth Bearer = (HttpBearerAuth) defaultClient.getAuthentication("Bearer");
+        Bearer.setBearerToken("BEARER TOKEN");
+
+        DefaultApi apiInstance = new DefaultApi(defaultClient);
+        
+        try {
+            KafkaRequestList result = apiInstance.getKafkas(null,null,null,null);
+            
+            assertNotNull(result);
+            assertEquals(1, result.getItems().size());
+        } catch (ApiException e) {
+            System.err.println("Exception when calling DefaultApi#createKafka");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+}
