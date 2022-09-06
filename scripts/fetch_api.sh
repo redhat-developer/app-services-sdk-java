4555fa00e475b8a719c05296f284bfebe417c176
Dimitri Saridakis <dimitri.saridakis@gmail.com>
2022-09-06 11:35:15 +0100

    fix(scripts): fixes the fetch api script (#356)

--- scripts/fetch_api.sh
+++ scripts/fetch_api.sh
@@ -5,7 +5,7 @@ OPENAPI_FILE_URL=$1
 OPENAPI_FILE_NAME=$(dirname $0)/../.openapi/$(basename $OPENAPI_FILE_URL)
 
 # download the OpenAPI file
-curl -H "Authorization: token $BF2_TOKEN" "$OPENAPI_FILE_URL" --output $OPENAPI_FILE_NAME
+curl "$OPENAPI_FILE_URL" --output $OPENAPI_FILE_NAME
 if [ $? != 0 ]; then
   exit $?
 fi
