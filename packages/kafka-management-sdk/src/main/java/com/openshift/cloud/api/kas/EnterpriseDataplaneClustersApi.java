1057105c0e5b9a349a18acb5a4dffe4ffe0c9836
app-services-ci <app-services-ci@users.noreply.github.com>
2023-01-09 16:36:29 +0000

    fix(kafka-mgmt/v1): update kafka-mgmt/v1 SDK

--- packages/kafka-management-sdk/src/main/java/com/openshift/cloud/api/kas/EnterpriseDataplaneClustersApi.java
+++ packages/kafka-management-sdk/src/main/java/com/openshift/cloud/api/kas/EnterpriseDataplaneClustersApi.java
@@ -39,6 +39,59 @@ public class EnterpriseDataplaneClustersApi {
 
   /**
    * 
+   * 
+   * @param async Perform the action in an asynchronous manner (required)
+   * @param id ID of the enterprise data plane cluster (required)
+   * @param force When provided with value: true - enterprise cluster will be deleted alongside all kafkas present on the cluster. When skipped and enterprise cluster has any kafkas associated with it, the request will fail. (optional)
+   * @return a {@code Error}
+   * @throws ApiException if fails to make API call
+   */
+  public Error deleteEnterpriseClusterById(Boolean async, String id, Boolean force) throws ApiException {
+    Object localVarPostBody = null;
+    
+    // verify the required parameter 'async' is set
+    if (async == null) {
+      throw new ApiException(400, "Missing the required parameter 'async' when calling deleteEnterpriseClusterById");
+    }
+    
+    // verify the required parameter 'id' is set
+    if (id == null) {
+      throw new ApiException(400, "Missing the required parameter 'id' when calling deleteEnterpriseClusterById");
+    }
+    
+    // create path and map variables
+    String localVarPath = "/api/kafkas_mgmt/v1/clusters/{id}".replaceAll("\\{format\\}","json")
+      .replaceAll("\\{" + "id" + "\\}", apiClient.escapeString(id.toString()));
+
+    // query params
+    List<Pair> localVarQueryParams = new ArrayList<Pair>();
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+    Map<String, String> localVarCookieParams = new HashMap<String, String>();
+    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "async", async));
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "force", force));
+
+    
+    
+    
+    final String[] localVarAccepts = {
+      "application/json"
+    };
+    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+
+    final String[] localVarContentTypes = {
+      
+    };
+    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+    String[] localVarAuthNames = new String[] { "Bearer" };
+
+    GenericType<Error> localVarReturnType = new GenericType<Error>() {};
+    return apiClient.invokeAPI(localVarPath, "DELETE", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+      }
+  /**
+   * 
    * List all Enterprise OSD clusters
    * @return a {@code EnterpriseClusterList}
    * @throws ApiException if fails to make API call
