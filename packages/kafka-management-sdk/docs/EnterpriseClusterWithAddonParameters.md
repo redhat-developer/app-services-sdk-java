de89dee61478609894ba3134ec71498f956f9c31
app-services-ci <app-services-ci@users.noreply.github.com>
2023-01-18 08:23:09 +0000

    fix(kafka-mgmt/v1): update kafka-mgmt/v1 SDK

---
+++ packages/kafka-management-sdk/docs/EnterpriseClusterWithAddonParameters.md
@@ -0,0 +1,20 @@
+
+
+# EnterpriseClusterWithAddonParameters
+
+Enterprise cluster with addon parameters
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**id** | **String** |  | 
+**kind** | **String** |  | 
+**href** | **String** |  | 
+**accessKafkasViaPrivateNetwork** | **Boolean** | Indicates whether Kafkas created on this data plane cluster have to be accessed via private network | 
+**clusterId** | **String** | OCM cluster id of the registered Enterprise cluster |  [optional]
+**status** | **String** | status of registered Enterprise cluster |  [optional]
+**fleetshardParameters** | [**List&lt;FleetshardParameter&gt;**](FleetshardParameter.md) |  |  [optional]
+
+
+
--- packages/kafka-management-sdk/src/main/java/com/openshift/cloud/api/kas/EnterpriseDataplaneClustersApi.java
+++ packages/kafka-management-sdk/src/main/java/com/openshift/cloud/api/kas/EnterpriseDataplaneClustersApi.java
@@ -7,8 +7,9 @@ import com.openshift.cloud.api.kas.invoker.Pair;
 
 import javax.ws.rs.core.GenericType;
 
+import com.openshift.cloud.api.kas.models.EnterpriseCluster;
 import com.openshift.cloud.api.kas.models.EnterpriseClusterList;
-import com.openshift.cloud.api.kas.models.EnterpriseClusterRegistrationResponse;
+import com.openshift.cloud.api.kas.models.EnterpriseClusterWithAddonParameters;
 import com.openshift.cloud.api.kas.models.EnterpriseOsdClusterPayload;
 import com.openshift.cloud.api.kas.models.Error;
 
@@ -92,6 +93,94 @@ public class EnterpriseDataplaneClustersApi {
       }
   /**
    * 
+   * Returns enterprise data plane cluster by ID
+   * @param id ID of the enterprise data plane cluster (required)
+   * @return a {@code EnterpriseCluster}
+   * @throws ApiException if fails to make API call
+   */
+  public EnterpriseCluster getEnterpriseClusterById(String id) throws ApiException {
+    Object localVarPostBody = null;
+    
+    // verify the required parameter 'id' is set
+    if (id == null) {
+      throw new ApiException(400, "Missing the required parameter 'id' when calling getEnterpriseClusterById");
+    }
+    
+    // create path and map variables
+    String localVarPath = "/api/kafkas_mgmt/v1/clusters/{id}".replaceAll("\\{format\\}","json")
+      .replaceAll("\\{" + "id" + "\\}", apiClient.escapeString(id.toString()));
+
+    // query params
+    List<Pair> localVarQueryParams = new ArrayList<Pair>();
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+    Map<String, String> localVarCookieParams = new HashMap<String, String>();
+    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+
+    
+    
+    
+    final String[] localVarAccepts = {
+      "application/json"
+    };
+    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+
+    final String[] localVarContentTypes = {
+      
+    };
+    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+    String[] localVarAuthNames = new String[] { "Bearer" };
+
+    GenericType<EnterpriseCluster> localVarReturnType = new GenericType<EnterpriseCluster>() {};
+    return apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+      }
+  /**
+   * 
+   * Returns enterprise data plane cluster by ID along with its addon parameters
+   * @param id ID of the enterprise data plane cluster (required)
+   * @return a {@code EnterpriseClusterWithAddonParameters}
+   * @throws ApiException if fails to make API call
+   */
+  public EnterpriseClusterWithAddonParameters getEnterpriseClusterWithAddonParameters(String id) throws ApiException {
+    Object localVarPostBody = null;
+    
+    // verify the required parameter 'id' is set
+    if (id == null) {
+      throw new ApiException(400, "Missing the required parameter 'id' when calling getEnterpriseClusterWithAddonParameters");
+    }
+    
+    // create path and map variables
+    String localVarPath = "/api/kafkas_mgmt/v1/clusters/{id}/addon_parameters".replaceAll("\\{format\\}","json")
+      .replaceAll("\\{" + "id" + "\\}", apiClient.escapeString(id.toString()));
+
+    // query params
+    List<Pair> localVarQueryParams = new ArrayList<Pair>();
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+    Map<String, String> localVarCookieParams = new HashMap<String, String>();
+    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+
+    
+    
+    
+    final String[] localVarAccepts = {
+      "application/json"
+    };
+    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+
+    final String[] localVarContentTypes = {
+      
+    };
+    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+    String[] localVarAuthNames = new String[] { "Bearer" };
+
+    GenericType<EnterpriseClusterWithAddonParameters> localVarReturnType = new GenericType<EnterpriseClusterWithAddonParameters>() {};
+    return apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+      }
+  /**
+   * 
    * List all Enterprise data plane clusters
    * @return a {@code EnterpriseClusterList}
    * @throws ApiException if fails to make API call
@@ -131,10 +220,10 @@ public class EnterpriseDataplaneClustersApi {
    * 
    * Register enterprise data plane cluster
    * @param enterpriseOsdClusterPayload Enterprise data plane cluster details (required)
-   * @return a {@code EnterpriseClusterRegistrationResponse}
+   * @return a {@code EnterpriseClusterWithAddonParameters}
    * @throws ApiException if fails to make API call
    */
-  public EnterpriseClusterRegistrationResponse registerEnterpriseOsdCluster(EnterpriseOsdClusterPayload enterpriseOsdClusterPayload) throws ApiException {
+  public EnterpriseClusterWithAddonParameters registerEnterpriseOsdCluster(EnterpriseOsdClusterPayload enterpriseOsdClusterPayload) throws ApiException {
     Object localVarPostBody = enterpriseOsdClusterPayload;
     
     // verify the required parameter 'enterpriseOsdClusterPayload' is set
@@ -167,7 +256,7 @@ public class EnterpriseDataplaneClustersApi {
 
     String[] localVarAuthNames = new String[] { "Bearer" };
 
-    GenericType<EnterpriseClusterRegistrationResponse> localVarReturnType = new GenericType<EnterpriseClusterRegistrationResponse>() {};
+    GenericType<EnterpriseClusterWithAddonParameters> localVarReturnType = new GenericType<EnterpriseClusterWithAddonParameters>() {};
     return apiClient.invokeAPI(localVarPath, "POST", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
       }
 }
